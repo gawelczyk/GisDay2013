312992de01fe1ed109368e088ae2f43ae6bc0872
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2013-11-15 21:41:02 +0100

    map is visible

--- GisDay2013/Scripts/myApp.js
+++ GisDay2013/Scripts/myApp.js
@@ -1,1 +1,21 @@
-﻿
+﻿var map;
+var MyApp = function () { };
+
+function init() {
+    map = new OpenLayers.Map({
+        div: "map",
+        projection: "EPSG:900913",
+        displayProjection: "EPSG:4326"
+    });
+
+    layer = new OpenLayers.Layer.OSM("Simple OSM Map");
+
+    map.addLayer(layer);
+    map.zoomToMaxExtent();
+
+    map.addControl(new OpenLayers.Control.MousePosition());
+}
+
+$(function () {
+    init();
+});
